d0932ae33d3934e2ff43d06ee81bc1d5905d239b
jaf7 <janthonyfields@gmail.com>
2018-01-17 11:44:54 -0500

    Built while loops using subtract-and-assign & sleep

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
+# require 'pry'
 
-def countdown
+def countdown(seconds)
+  while seconds > 0 do
+    phrase = "#{seconds} SECOND(S)!"
+    puts phrase
+    seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  while seconds > 0 do
+    phrase = "#{seconds} SECOND(S)!"
+    puts phrase
+    seconds -= 1
+    sleep(1)
+  end
 end
